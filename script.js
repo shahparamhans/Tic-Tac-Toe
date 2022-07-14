ec9caecb75808a496d03e2bd9a02b8062957dbfc
Paramhans Shah <shahparamhans@gmail.com>
2022-07-14 15:29:37 +0530

    Initial commit

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const changeTurn = () =>{
 const checkWin = () =>{
     let boxtext = document.getElementsByClassName('boxtext');
     let wins = [
-        [0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]
+        [0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]
     ]
     wins.forEach(e=>{
         if((boxtext[e[0]].innerText===boxtext[e[1]].innerText) && (boxtext[e[1]].innerText===boxtext[e[2]].innerText) && (boxtext[e[0]].innerText!=="")){
